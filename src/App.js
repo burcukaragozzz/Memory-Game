ec69a32b173e84e8dd5f5be7d51b088c4f85b386
burcukaragozzz <burcu.karagoz@ytu-burcu-karagozs-MacBook-Air.local>
2019-09-16 16:58:31 +0300

    Fixed PR commits

--- src/App.js
+++ src/App.js
@@ -11,12 +11,11 @@ class App extends Component {
       firstSelectedItem: null,
       secondSelectedItem: null,
       selectedCount: 0,
-      score: 225,
-	  text: '',
-	  openedCard: 0,
-	  angle: null,
+      score: 230,
+	  	openedCard: 0,
+	  	angle: null,
     };
-  }
+	}
 
   onClicked = (itemValue, itemId) => {
     const { firstSelectedItem } = this.state;
@@ -31,7 +30,7 @@ class App extends Component {
       }, () => {
 			if(this.state.firstSelectedItem.id !== itemId && firstSelectedItem.value === itemValue) {
 				setTimeout(() => {
-				this.increaseSelectedCount();
+					this.increaseSelectedCount();
 				}, 600);
 			} else {
 				this.updateScore();
@@ -43,78 +42,84 @@ class App extends Component {
           value: itemValue,
           id: itemId,
         },
-		openedCard: this.state.openedCard + 1,
+				openedCard: this.state.openedCard + 1,
       });
     }
-};
+	};
 
   increaseSelectedCount = () => {
-    const { selectedCount} = this.state;
-    this.setState({
-		firstSelectedItem: null,
-		secondSelectedItem: null,
-      	selectedCount: selectedCount + 2,
-		openedCard: 0,
-    });
-};
+		this.setState(prevstate => ({ 
+			firstSelectedItem: null,
+			secondSelectedItem: null,
+			selectedCount: prevstate.selectedCount + 2,
+			openedCard: 0,
+		}));
+	};
 
-  updateScore = () => {
-	const { score } = this.state;
-	let rotateAngle= 0;
+	updateScore = () => {
+		const { score } = this.state;
+		let rotateAngle= 0;
 
-	setTimeout(() => {
-		this.setState({
-		openedCard: 0,
-	    score: this.state.score - 5,
-		firstSelectedItem: null,
-		secondSelectedItem: null,
-	  });
-	}, 600);
+		if(score === 0){
+			this.setState({
+				score: 0,
+				angle: 40,
+				selectedCount: 16,
+			});
+		}else{
+			setTimeout(() => {
+				this.setState({
+					openedCard: 0,
+			    score: this.state.score - 10,
+					firstSelectedItem: null,
+					secondSelectedItem: null,
+			  });
+			}, 600);
 
-	rotateAngle= (score*1.8)-30;
-	this.setState({
-		angle: rotateAngle,
-    });
-};
+			rotateAngle= (score * 1.8)-20;
+			this.setState({
+				angle: rotateAngle,
+			});
+		}
+	};
 
   isSelected = cardItem => {
     const { firstSelectedItem, secondSelectedItem } = this.state;
     if (firstSelectedItem && secondSelectedItem && firstSelectedItem.id !== secondSelectedItem.id && firstSelectedItem.value === secondSelectedItem.value) {
-     return firstSelectedItem.value === cardItem.value || secondSelectedItem.value === cardItem.value 
+    	return firstSelectedItem.value === cardItem.value || secondSelectedItem.value === cardItem.value 
     }
-};
+	};
 
   renderGameOver = () => (
-	<div className="gameOver">
-		<div className="imageContainer">
-			<img className="resultScore" src="images/solo-test_2324.jpg" alt="backgroundImage"/>
+	<div className="game-score-container">
+		<figure className="game-score-table">
+			<img className="game-score-table__image" src="images/solo-test_2324.jpg" alt="backgroundImage"/>
+		</figure>
+		<div className="game-score-result-line" style={ { transform: `translate(-50%, -40%) rotate(${ this.state.angle }deg)`} }>
+			<div className="game-score-result-line__left-visible"></div>
+			<div className="game-score-result-line__right-hidden"></div>
 		</div>
-		<div className="resultLine" style={ { transform: `rotate(${ this.state.angle }deg)`} }>
-			<div className="line_1"></div>
-			<div className="line_2"></div>
-		</div>
-		<div className = "score">{this.state.text}</div>
 		<button onClick = {() => window.location.reload(false)}>New Game</button>
 	</div>
-   )
-  
-  render() {
-    const { firstSelectedItem, secondSelectedItem, selectedCount, numbers, openedCard } = this.state;
+  )
 
+  render() {
+		const { firstSelectedItem, secondSelectedItem, numbers, selectedCount, openedCard } = this.state;
+		const openedCardLength = 2;
+		const cardMaps = numbers.map((item, i) =>
+			<Card 
+				id={item.id} 
+			  value={item.value} 
+			  handleClick={() => openedCard === openedCardLength ? null : this.onClicked(item.value,item.id)} 
+		    selected={this.isSelected(item)}
+		    isVisible={(firstSelectedItem && firstSelectedItem.id === item.id ) || (secondSelectedItem && secondSelectedItem.id === item.id)}
+			/>
+		)
     return(
       <div className="content">
-        { selectedCount === numbers.length ? this.renderGameOver() : 
-		  (<div className="cards">{numbers.map((item, i) =>
-	          <Card 
-	             id={item.id} 
-	             value={item.value} 
-	             handleClick={() => openedCard === 2 ? null : this.onClicked(item.value,item.id)} 
-	             selected={this.isSelected(item)}
-	             isVisible={(firstSelectedItem && firstSelectedItem.id === item.id ) || (secondSelectedItem && secondSelectedItem.id === item.id)}
-	           />
-            )}</div>
-          )
-        }
+        { selectedCount === numbers.length ? this.renderGameOver() : 			
+					<div className="cards">{cardMaps}</div>
+				}
       </div>
     );
   }
